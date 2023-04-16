5961c24459d7c072529bbc853ea62f0d69a9cc5e
Professional <new99kpicem@gmail.com>
2023-04-16 17:01:21 +0300

    prc

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Hogwarts!");
-        Gryffindor GarryPotter = new Gryffindor("Гарри Поттер", 90, 100,100,90,90);
-        System.out.println("Студент: " + GarryPotter.getFullData());
-        Gryffindor GermionaGranger = new Gryffindor("Гермиона Грейнджер",80,100,100,100,80);
-        System.out.println("Студент: " + GermionaGranger.getFullData());
+        Gryffindor garryPotter = new Gryffindor("Гарри Поттер", 90, 100,100,90,90);
+        System.out.println("Студент: " + garryPotter.getFullData());
+        Gryffindor germionaGranger = new Gryffindor("Гермиона Грейнджер",80,80,100,100,80);
+        System.out.println("Студент: " + germionaGranger.getFullData());
+        Gryffindor ronWeasley = new Gryffindor("Рон Уизли", 70, 100, 90,80,80);
+        System.out.println("Студент: " + ronWeasley.getFullData());
+        Hufflepuff zahariaSmith = new Hufflepuff("Захария Смит", 100,90,80, 100, 70);
+        System.out.println("Студент: " + zahariaSmith.getFullData());
+        Hufflepuff sedrikDiggory = new Hufflepuff("Седрик Диггори", 90, 80, 70, 80, 80);
+        System.out.println("Студент: " + sedrikDiggory.getFullData());
+        Hufflepuff justinFinch = new Hufflepuff("Джастин Финч-Флетчли", 80, 80, 80, 90, 70);
+        System.out.println("Студент: " + justinFinch.getFullData());
     }
 }
